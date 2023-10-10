50cef30cbab57f302dc6a7725b9c59269f8925bd
Vicente Bartual <vicentebartualf@gmail.com>
2023-10-10 20:47:10 +0200

    Add AES 128 encryption

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+mod aes;
+
+
+fn print_hex(data: &[u8]) {
+    for i in 0..data.len() {
+        print!("{:02x}", data[i]);
+        if (i+1) % 4 == 0 {
+            print!(" ");
+        }
+    }
+    println!();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let data: [u8; 16] = "Attack at dawn!!".as_bytes().try_into().unwrap();
+    let key: [u8; 16] = "yellow submarine".as_bytes().try_into().unwrap();
+
+    let ciphertext = aes::aes_128_encrypt(&data, &key);
+    print_hex(ciphertext.as_slice());
 }
