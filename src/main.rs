e11eee02422db2b9e3e60002dd93a8a06315615a
Vicente Bartual <vicentebartualf@gmail.com>
2023-10-12 23:46:10 +0200

    Add AES 128 decryption

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,12 @@ fn main() {
     let data: [u8; 16] = "Attack at dawn!!".as_bytes().try_into().unwrap();
     let key: [u8; 16] = "yellow submarine".as_bytes().try_into().unwrap();
 
+    print_hex(data.as_slice());
+
     let ciphertext = aes::aes_128_encrypt(&data, &key);
     print_hex(ciphertext.as_slice());
+
+    let plaintext = aes::aes_128_decrypt(&ciphertext, &key);
+    print_hex(plaintext.as_slice());
+    assert_eq!(&data, plaintext.as_ref());
 }
