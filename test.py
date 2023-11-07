6acc237db520fbe5d7388c5f5f21d88aeb823144
Vicente Bartual <vicentebartualf@gmail.com>
2023-11-08 00:35:46 +0100

    Add python test script

---
+++ test.py
@@ -0,0 +1,45 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+
+
+DATA = b"Hey friends, this is a longer message!!!"
+KEY = b"yellow submarine"
+
+
+def print_bytes(data: bytes):
+    for i, b in enumerate(data):
+        print(f"{b:02x}", end="")
+        if (i+1) % 4 == 0:
+            print(" ", end="")
+    print()
+
+
+def aes_ecb(data, key: bytes):
+    cipher = AES.new(key, AES.MODE_ECB)
+    ct_bytes = cipher.encrypt(pad(data, AES.block_size))
+    print_bytes(ct_bytes)
+    print("AES-ECB")
+
+
+def aes_cbc(data, key: bytes, iv: bytes):
+    cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+    ct_bytes = cipher.encrypt(pad(data, AES.block_size))
+    print_bytes(ct_bytes)
+    print("AES-CBC")
+
+
+def aes_ctr(data, key: bytes, nonce):
+    cipher = AES.new(key, AES.MODE_CTR, nonce=nonce)
+    ct_bytes = cipher.encrypt(data)
+    print_bytes(ct_bytes)
+    print("AES-CTR")
+
+
+def main():
+    aes_ecb(DATA, KEY)
+    aes_cbc(DATA, KEY, bytes(16))
+    aes_ctr(DATA, KEY, bytes(8))
+
+
+if __name__ == "__main__":
+    main()
