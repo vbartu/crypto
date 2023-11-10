60d2ccd74b65dbc91599999b5ae483f4568ecda0
Vicente Bartual <vicentebartualf@gmail.com>
2023-11-10 02:56:15 +0100

    Make SHA functions accept generics

--- src/hash/sha2.rs
+++ src/hash/sha2.rs
@@ -1,5 +1,6 @@
 use super::Hash;
 use super::sha2_constants as constants;
+use std::ops::{BitAnd, BitXor, Not};
 
 
 pub struct Sha224 {
@@ -9,9 +10,11 @@ pub struct Sha224 {
     total_data: usize,
 }
 
-impl ShaCommon for Sha224 {
+impl ShaCommon<u32> for Sha224 {
     const DIGEST_SIZE: usize = constants::SHA224_DIGEST_SIZE;
     const BLOCK_SIZE: usize = constants::SHA256_BLOCK_SIZE;
+    const W_LENGTH: usize = constants::SHA256_W_LENGTH;
+    const K_CONST: &'static[u32] = &constants::SHA256_K;
 
     fn new() -> Self {
         Self {
@@ -59,9 +62,11 @@ pub struct Sha256 {
     total_data: usize,
 }
 
-impl ShaCommon for Sha256 {
+impl ShaCommon<u32> for Sha256 {
     const DIGEST_SIZE: usize = constants::SHA256_DIGEST_SIZE;
     const BLOCK_SIZE: usize = constants::SHA256_BLOCK_SIZE;
+    const W_LENGTH: usize = constants::SHA256_W_LENGTH;
+    const K_CONST: &'static[u32] = &constants::SHA256_K;
 
     fn new() -> Self {
         Self {
@@ -102,11 +107,13 @@ impl ShaCommon for Sha256 {
     }
 }
 
-trait ShaCommon{
+trait ShaCommon<T: 'static> {
     const BLOCK_SIZE: usize;
     const DIGEST_SIZE: usize;
+    const W_LENGTH: usize;
+    const K_CONST: &'static[T];
     fn new() -> Self;
-    fn hash(&mut self) -> &mut [u32; 8];
+    fn hash(&mut self) -> &mut [T; 8];
     fn data(&mut self) -> &mut [u8]; // Size MUST BE BLOCK_SIZE
     fn get_pending(&mut self) -> usize;
     fn set_pending(&mut self, value: usize);
@@ -115,11 +122,11 @@ trait ShaCommon{
     fn reset(&mut self);
 }
 
-impl <T: ShaCommon> Hash for T {
-    const DIGEST_SIZE: usize = <T as ShaCommon>::DIGEST_SIZE;
+impl <T: ShaCommon<u32>> Hash for T {
+    const DIGEST_SIZE: usize = <T as ShaCommon<u32>>::DIGEST_SIZE;
 
     fn new() -> Self {
-        <T as ShaCommon>::new()
+        <T as ShaCommon<u32>>::new()
     }
 
     fn update(&mut self, data: &[u8]) {
@@ -134,13 +141,13 @@ impl <T: ShaCommon> Hash for T {
         //TODO: unsafe to dereference data?
         let mut old_data = self.data().to_vec();
         old_data[pending..].copy_from_slice(&data[..missing_data]);
-        process_block(&mut self.hash(), &old_data);
+        process_block(&mut self.hash(), &old_data, &Self::K_CONST, Self::W_LENGTH);
         self.inc_total(Self::BLOCK_SIZE);
 
         let block_iter = data[missing_data..].chunks_exact(Self::BLOCK_SIZE);
         let rem = block_iter.remainder();
         for block in block_iter {
-            process_block(&mut self.hash(), block);
+            process_block(&mut self.hash(), block, &Self::K_CONST, Self::W_LENGTH);
             self.inc_total(Self::BLOCK_SIZE);
         }
 
@@ -152,9 +159,9 @@ impl <T: ShaCommon> Hash for T {
         self.update(data);
         let pending = self.get_pending();
         let total = self.get_total();
-        let padded = pad_last_block(&self.data()[..pending], total);
+        let padded = pad_last_block(&self.data()[..pending], total, Self::BLOCK_SIZE);
         for block in padded.chunks_exact(Self::BLOCK_SIZE) {
-            process_block(&mut self.hash(), block);
+            process_block(&mut self.hash(), block, &Self::K_CONST, Self::W_LENGTH);
         }
         let mut digest: Vec<u8> = Vec::with_capacity(Self::DIGEST_SIZE);
         for i in 0..Self::DIGEST_SIZE/4 {
@@ -165,40 +172,92 @@ impl <T: ShaCommon> Hash for T {
     }
 }
 
-fn ch(x: u32, y: u32, z: u32) -> u32 {
-    (x & y) ^ (!x & z)
+trait ShaUnsg: Copy
+             + BitAnd<Output = Self>
+             + BitXor<Output = Self>
+             + Not<Output = Self>
+{
+    fn s0(self) -> Self;
+    fn s1(self) -> Self;
+    fn v0(self) -> Self;
+    fn v1(self) -> Self;
+    fn wrapping_add(self, lhs: Self) -> Self;
+    fn from_be_bytes(bytes: &[u8]) -> Self;
+
+    fn ch(x: Self, y: Self, z: Self) -> Self
+    {
+        (x & y) ^ (!x & z)
+    }
+
+    fn maj(x: Self, y: Self, z: Self) -> Self
+    {
+        (x & y) ^ (x & z) ^ (y & z)
+    }
 }
 
-fn maj(x: u32, y: u32, z: u32) -> u32 {
-    (x & y) ^ (x & z) ^ (y & z)
+impl ShaUnsg for u32 {
+    fn s0(self) -> Self {
+        self.rotate_right(2) ^ self.rotate_right(13) ^ self.rotate_right(22)
+    }
+
+    fn s1(self) -> Self {
+        self.rotate_right(6) ^ self.rotate_right(11) ^ self.rotate_right(25)
+    }
+
+    fn v0(self) -> Self {
+        self.rotate_right(7) ^ self.rotate_right(18) ^ (self >> 3)
+    }
+
+    fn v1(self) -> Self {
+        self.rotate_right(17) ^ self.rotate_right(19) ^ (self >> 10)
+    }
+
+    fn wrapping_add(self, lhs: Self) -> Self {
+        self.wrapping_add(lhs)
+    }
+
+    fn from_be_bytes(bytes: &[u8]) -> Self {
+        u32::from_be_bytes(bytes.try_into().unwrap())
+    }
 }
 
-fn s0(x: u32) -> u32 {
-    x.rotate_right(2) ^ x.rotate_right(13) ^ x.rotate_right(22)
+impl ShaUnsg for u64 {
+    fn s0(self) -> Self {
+        self.rotate_right(28) ^ self.rotate_right(34) ^ self.rotate_right(39)
+    }
+
+    fn s1(self) -> Self {
+        self.rotate_right(14) ^ self.rotate_right(18) ^ self.rotate_right(41)
+    }
+
+    fn v0(self) -> Self {
+        self.rotate_right(1) ^ self.rotate_right(8) ^ (self >> 7)
+    }
+
+    fn v1(self) -> Self {
+        self.rotate_right(19) ^ self.rotate_right(61) ^ (self >> 6)
+    }
+
+    fn wrapping_add(self, lhs: Self) -> Self {
+        self.wrapping_add(lhs)
+    }
+
+    fn from_be_bytes(bytes: &[u8]) -> Self {
+        u64::from_be_bytes(bytes.try_into().unwrap())
+    }
 }
 
-fn s1(x: u32) -> u32 {
-    x.rotate_right(6) ^ x.rotate_right(11) ^ x.rotate_right(25)
-}
-
-fn v0(x: u32) -> u32 {
-    x.rotate_right(7) ^ x.rotate_right(18) ^ (x >> 3)
-}
-
-fn v1(x: u32) -> u32 {
-    x.rotate_right(17) ^ x.rotate_right(19) ^ (x >> 10)
-}
-
-fn pad_last_block(block: &[u8], total_data: usize) -> Vec<u8> {
-    assert!(block.len() <= constants::SHA256_BLOCK_SIZE);
-    let mut padded = Vec::with_capacity(constants::SHA256_BLOCK_SIZE);
+fn pad_last_block(block: &[u8], total_data: usize,
+                  block_size: usize) -> Vec<u8> {
+    assert!(block.len() <= block_size);
+    let mut padded = Vec::with_capacity(block_size);
     padded.extend_from_slice(block);
     padded.push(0x80);
     let k: usize;
     if block.len() < 56 {
-        k = constants::SHA256_BLOCK_SIZE - block.len() - 1 - 8;
+        k = block_size - block.len() - 1 - block_size/8;
     } else {
-        k = constants::SHA256_BLOCK_SIZE*2 - block.len() - 1 - 8;
+        k = block_size*2 - block.len() - 1 - block_size/8;
     }
     for _ in 0..k {
         padded.push(0)
@@ -208,32 +267,35 @@ fn pad_last_block(block: &[u8], total_data: usize) -> Vec<u8> {
     padded
 }
 
-fn process_block(hash: &mut [u32; 8], block: &[u8]) {
+fn process_block<T: ShaUnsg>(hash: &mut [T; 8], block: &[u8],
+                             k_const: &[T], w_length: usize) {
     // Message schedule (W)
-    let mut w: [u32; 64] = [0; 64];
-    for i in 0..16 {
-        let index = 4*i;
-        w[i] = u32::from_be_bytes(block[index..index+4].try_into().unwrap());
+    let mut w: Vec<T> = Vec::with_capacity(w_length);
+    
+    // Always 16 rounds
+    for int_bytes in block.chunks(std::mem::size_of::<T>()) {
+        w.push(T::from_be_bytes(int_bytes));
     }
-    for i in 16..64 {
-        w[i] = v1(w[i-2])
+    for i in 16..w_length {
+        w.push(T::v1(w[i-2])
             .wrapping_add(w[i-7])
-            .wrapping_add(v0(w[i-15]))
-            .wrapping_add(w[i-16]);
+            .wrapping_add(T::v0(w[i-15]))
+            .wrapping_add(w[i-16]));
     }
 
     // Working variables: a(0), b(1), c(2), d(3), e(4), f(5), g(6), h(7)
     let mut v = hash.clone();
 
-    for i in 0..w.len() {
+    for i in 0..w_length {
         // t1 = h + s1(e) + ch(e, f, g) + K[t] +  w[t]
-        let t1: u32 = v[7]
-            .wrapping_add(s1(v[4]))
-            .wrapping_add(ch(v[4], v[5], v[6]))
-            .wrapping_add(constants::SHA256_K[i])
+        let t1: T = v[7]
+            .wrapping_add(T::s1(v[4]))
+            .wrapping_add(T::ch(v[4], v[5], v[6]))
+            .wrapping_add(k_const[i])
             .wrapping_add(w[i]);
         // t2 = s0(a) + maj(a, b, c);
-        let t2: u32 = s0(v[0]).wrapping_add(maj(v[0], v[1], v[2]));
+        let t2: T = T::s0(v[0]).wrapping_add(T::maj(v[0], v[1], v[2]));
+
         v[7] = v[6];                  // h = g;
         v[6] = v[5];                  // g = f;
         v[5] = v[4];                  // f = e;
--- src/hash/sha2_constants.rs
+++ src/hash/sha2_constants.rs
@@ -16,8 +16,11 @@ pub const SHA256_H: [u32; 8] = [
     0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
 ];
 
+// Message schedule (W) length
+pub const SHA256_W_LENGTH: usize = 64;
+
 // K constants
-pub const SHA256_K: [u32; 64] = [
+pub const SHA256_K: [u32; SHA256_W_LENGTH] = [
    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
    0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
